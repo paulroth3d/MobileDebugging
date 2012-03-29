b6c24b158526e6256b1220df454c582954282aa5
Paul Roth <paulroth3d@gmail.com>
2012-03-29 03:48:54 -0500

    CLEANUP: add in additional notes for easier reading

--- code/Mobile Debug/Mobile Debug/www/app.js
+++ code/Mobile Debug/Mobile Debug/www/app.js
@@ -9,42 +9,47 @@
 
 //<debug>
 Ext.Loader.setPath({
-    'Ext': 'sdk/src'
+	'Ext': 'sdk/src'
 });
 //</debug>
 
 var senchaTalkApp = Ext.application({
-    name: 'senchaTalk',
-    apiVersion: 'v23.0',
+	name: 'senchaTalk',
+	apiVersion: 'v23.0',
 
-    requires: [
-        'Ext.MessageBox'
-    ],
+	requires: [
+		'Ext.MessageBox'
+	],
 
-    views: ['Main','Home','SFDC','ContactMe', 'LayoutExample', 'Docking', 'TemplateDemo' ],
-    
-    controllers: [ 'Contact', 'SfdcController' ],
+	views: ['Main','Home','SFDC','ContactMe', 'LayoutExample', 'Docking', 'TemplateDemo' ],
+	
+	controllers: [ 'Contact', 'SfdcController' ],
 
-    icon: {
-        57: 'resources/icons/Icon.png',
-        72: 'resources/icons/Icon~ipad.png',
-        114: 'resources/icons/Icon@2x.png',
-        144: 'resources/icons/Icon~ipad@2x.png'
-    },
-    
-    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
-    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
-
-    launch: function() {
-    	var self = this;
-    	
-    	console.log( "get auth credentials");
-        SalesforceOAuthPlugin.getAuthCredentials(
-        	function( creds ){
-        		console.log( 'getAuthCredentials returned' );
-        		console.log( creds );
-        		console.log( Ext.encode( creds ) );
-        		
+	icon: {
+		57: 'resources/icons/Icon.png',
+		72: 'resources/icons/Icon~ipad.png',
+		114: 'resources/icons/Icon@2x.png',
+		144: 'resources/icons/Icon~ipad@2x.png'
+	},
+	
+	phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+	tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+	
+	/**
+	 *  Launch is the initial function when the application is ready
+	**/
+	launch: function() {
+		var self = this;
+		
+		//-- now we get the auth credentials to connect to salesforce
+		//-- see _oldIndex.html for the standard file
+		console.log( "get auth credentials");
+		SalesforceOAuthPlugin.getAuthCredentials(
+			function( creds ){
+				console.log( 'getAuthCredentials returned' );
+				console.log( creds );
+				console.log( Ext.encode( creds ) );
+				
 				forcetkClient = new forcetk.Client(creds.clientId, creds.loginUrl);
 				forcetkClient.setSessionToken(creds.accessToken, this.apiVersion, creds.instanceUrl);
 				forcetkClient.setRefreshToken(creds.refreshToken);
@@ -55,39 +60,44 @@ var senchaTalkApp = Ext.application({
 				console.log( "Error occurred while loading credentials" );
 			}
 		);
-    },
-    
-    appReady: function(){
-    	console.log( "--- APP READY called----" );
-    	
-    	// Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
+	},
+	
+	/**
+	 *  App Ready is a custom method that is called when everything is complete
+	**/
+	appReady: function(){
+		
+		console.log( "--- APP READY called----" );
+		
+		// Destroy the #appLoadingIndicator element
+		Ext.fly('appLoadingIndicator').destroy();
 
-        // Initialize the main view
-        Ext.Viewport.add(Ext.create('senchaTalk.view.Main'));
-    },
+		// Initialize the main view
+		Ext.Viewport.add(Ext.create('senchaTalk.view.Main'));
+	},
 
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function() {
-                window.location.reload();
-            }
-        );
-    },
-    
+	onUpdated: function() {
+		Ext.Msg.confirm(
+			"Application Update",
+			"This application has just successfully been updated to the latest version. Reload now?",
+			function() {
+				window.location.reload();
+			}
+		);
+	},
+	
+	//-- custom methods added to the controller
 	/**
 	 *  Custom method to show notification, but provide fallback if not on ios
 	 *  @param message
 	 *  @param callback
 	 *  @param title
 	**/
-    showNotification: function( message, callback, title ){
-    	if( callback == null ) callback = Ext.emptyFn;
-    	if( title == null ) title = '';
-    	
-    	if( navigator && navigator.notification && navigator.notification.alert ){
+	showNotification: function( message, callback, title ){
+		if( callback == null ) callback = Ext.emptyFn;
+		if( title == null ) title = '';
+		
+		if( navigator && navigator.notification && navigator.notification.alert ){
 			navigator.notification.alert(
 				message,			// message
 				callback,			// callback
@@ -98,5 +108,5 @@ var senchaTalkApp = Ext.application({
 			alert( message );
 			callback();
 		}
-    }
+	}
 });
